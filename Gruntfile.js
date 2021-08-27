9b95b55df856d10115627d2b949e04f386292978
Heaven Nguyen <huongnguyen10102@gmail.com>
2021-08-27 08:28:50 +0300

    grunt p1

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('time-grunt')(grunt);
+
+    require('jit-grunt')(grunt);
+
+    const sass = require('node-sass');
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                implementation: sass,
+                outputStyle: 'expanded',
+                sourceMap: true,
+                quiet: true // stop depreciation errors
+            },
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        },
+
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: './'
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('css',['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch'] );
+}
